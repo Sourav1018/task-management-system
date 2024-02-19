a9fd49a24d4759a8374ca7a939edfeb84e33cb35
Sourav Ghosh @Crowntail <sourav.ghosh@crowntailtech.com>
2024-02-20 03:27:32 +0530

    -- page setup done and side bar is complete

--- src/components/LeftDrawer.vue
+++ src/components/LeftDrawer.vue
@@ -6,21 +6,47 @@
     bordered
     side="left"
   >
-    <q-list>
-      <div class="row no-wrap q-pa-sm">
-        <q-img
-          src="https://img.freepik.com/free-vector/illustration-circle-stamp-banner-vector_53876-27183.jpg?w=740&t=st=1708293617~exp=1708294217~hmac=7f727a39067d71259b3c7632abcf34e743819ab16fc43cbd81b4890cdde58ef9"
-        />
-      </div>
-      <q-separator inset />
-      <q-item clickable>
-        <q-item-section avatar>
-          <q-icon name="mdi-calendar-check-outline" color="grey" />
+    <div class="row no-wrap justify-center q-pa-none q-my-sm">
+      <q-img
+        src="https://img.freepik.com/free-vector/illustration-circle-stamp-banner-vector_53876-27183.jpg?w=740&t=st=1708293617~exp=1708294217~hmac=7f727a39067d71259b3c7632abcf34e743819ab16fc43cbd81b4890cdde58ef9"
+      />
+    </div>
+
+    <q-separator class="q-my-sm" inset />
+
+    <q-list class="flex flex-center">
+      <q-item
+        clickable
+        class="q-pa-none"
+        exact
+        to="/task"
+        active-class="active-element"
+        @click="selected"
+      >
+        <q-item-section avatar class="q-pa-none flex-center">
+          <q-icon name="fa-solid fa-list-check" color="grey-10" />
         </q-item-section>
-        <q-separator />
       </q-item>
     </q-list>
-    <!-- <q-item clickable v-ripple> </q-item> -->
+    <div class="drawer-footer">
+      <div>
+        <q-item clickable to="/profile" exact active-class="active-element">
+          <q-item-section avatar>
+            <q-icon name="fa-solid fa-user" color="grey-10" />
+          </q-item-section>
+        </q-item>
+      </div>
+
+      <q-separator class="q-my-sm" inset />
+
+      <div>
+        <q-item clickable @click="signOut">
+          <q-item-section avatar>
+            <q-icon name="fa-solid fa-right-from-bracket" color="grey-10" />
+          </q-item-section>
+        </q-item>
+      </div>
+    </div>
   </q-drawer>
 </template>
 
@@ -38,10 +64,34 @@ export default defineComponent({
     const drawerWidth = computed(() => {
       return $q.screen.xs ? 80 : 70;
     });
+
+    function signOut() {
+      console.log("logout sucessfully");
+    }
+
+    const selected = () => {};
     return {
       miniState,
       drawerWidth,
+      signOut,
     };
   },
 });
 </script>
+
+<style scoped>
+.drawer-footer {
+  position: absolute;
+  bottom: 20px;
+  margin-left: 8px;
+  margin-right: 8px;
+  left: 0;
+  right: 0;
+  text-align: center;
+}
+
+.active-element {
+  background: #4285f4;
+  border-radius: 20px;
+}
+</style>
