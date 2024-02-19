6a7ed5c194994ef485ee2478767d2c7c0705474c
Sourav Ghosh @Crowntail <sourav.ghosh@crowntailtech.com>
2024-02-19 10:28:27 +0530

    -- logo adjustment

--- src/components/LeftDrawer.vue
+++ src/components/LeftDrawer.vue
@@ -1,24 +1,28 @@
 <template>
   <q-drawer
     show-if-above
-    mini
     :breakpoint="500"
     :width="drawerWidth"
     bordered
     side="left"
   >
-    <q-item>
-      <q-item-section avatar>
-        <q-avatar rounded>
-          <img
-            src="https://img.freepik.com/free-vector/illustration-circle-stamp-banner-vector_53876-27183.jpg?w=740&t=st=1708293617~exp=1708294217~hmac=7f727a39067d71259b3c7632abcf34e743819ab16fc43cbd81b4890cdde58ef9"
-          />
-        </q-avatar>
-      </q-item-section>
-      <q-item-section avatar>
-        <q-icon name="mdi-calendar-check-outline" color="grey" />
-      </q-item-section>
-    </q-item>
+    <q-list>
+      <q-item>
+        <q-item-section class="">
+          <q-avatar rounded>
+            <q-img
+              src="https://img.freepik.com/free-vector/illustration-circle-stamp-banner-vector_53876-27183.jpg?w=740&t=st=1708293617~exp=1708294217~hmac=7f727a39067d71259b3c7632abcf34e743819ab16fc43cbd81b4890cdde58ef9"
+            />
+          </q-avatar>
+        </q-item-section>
+      </q-item>
+      <q-item clickable>
+        <q-item-section avatar>
+          <q-icon name="mdi-calendar-check-outline" color="grey" />
+        </q-item-section>
+        <q-separator />
+      </q-item>
+    </q-list>
     <!-- <q-item clickable v-ripple> </q-item> -->
   </q-drawer>
 </template>
@@ -35,7 +39,7 @@ export default defineComponent({
     const miniState = ref(true);
     const $q = useQuasar();
     const drawerWidth = computed(() => {
-      return $q.screen.xs ? 80 : 57;
+      return $q.screen.xs ? 80 : 70;
     });
     return {
       miniState,
