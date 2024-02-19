f16bbe2387a4b702d0195cb8532190863581d9b0
Sourav Ghosh @Crowntail <sourav.ghosh@crowntailtech.com>
2024-02-19 10:32:38 +0530

    -- logo position and list icon WIP

--- src/components/LeftDrawer.vue
+++ src/components/LeftDrawer.vue
@@ -7,15 +7,12 @@
     side="left"
   >
     <q-list>
-      <q-item>
-        <q-item-section class="">
-          <q-avatar rounded>
-            <q-img
-              src="https://img.freepik.com/free-vector/illustration-circle-stamp-banner-vector_53876-27183.jpg?w=740&t=st=1708293617~exp=1708294217~hmac=7f727a39067d71259b3c7632abcf34e743819ab16fc43cbd81b4890cdde58ef9"
-            />
-          </q-avatar>
-        </q-item-section>
-      </q-item>
+      <div class="row no-wrap q-pa-sm">
+        <q-img
+          src="https://img.freepik.com/free-vector/illustration-circle-stamp-banner-vector_53876-27183.jpg?w=740&t=st=1708293617~exp=1708294217~hmac=7f727a39067d71259b3c7632abcf34e743819ab16fc43cbd81b4890cdde58ef9"
+        />
+      </div>
+      <q-separator inset />
       <q-item clickable>
         <q-item-section avatar>
           <q-icon name="mdi-calendar-check-outline" color="grey" />
